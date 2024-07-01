ba3934cfe968f12b01d0cac56e428b2156038d12
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-01 17:06:10 +0800

    Include quantity and addToCart handler

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -5,7 +5,7 @@ const initialState = localStorage.getItem("cart")
   : {cartItems: []};
 
 const addDecimals = (num) => {
-  return Math.round((num * 100) / 100).toFixed(2);
+  return (Math.round(num * 100) / 100).toFixed(2);
 }
 
 const cartSlice = createSlice({
@@ -26,7 +26,7 @@ const cartSlice = createSlice({
       //Calculate items price
       state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
 
-      //Calculate shipping price (If order is over $100 then free, else $10 shipping)
+      //Calculate shipping price (If order is over ₱100 then free, else ₱10 shipping)
       state.shippingPrice =  addDecimals(state.itemsPrice > 100 ? 0 : 10);
 
       //Calculate tax price (15% tax)
