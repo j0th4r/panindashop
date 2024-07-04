5451ab68bed5270e7beb91bee890169b2e6bf6b0
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-04 11:40:22 +0800

    Setup paypal and order paid

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -76,22 +76,22 @@ const OrderScreen = () => {
 
               <ListGroup.Item>
                 <Row>
-                  <Col>Items</Col>
+                  <Col>Items:</Col>
                   <Col>₱{order.itemsPrice}</Col>
                 </Row>
 
                 <Row>
-                  <Col>Shipping</Col>
+                  <Col>Shipping:</Col>
                   <Col>₱{order.shippingPrice}</Col>
                 </Row>
 
                 <Row>
-                  <Col>Tax</Col>
+                  <Col>Tax:</Col>
                   <Col>₱{order.taxPrice}</Col>
                 </Row>
 
                 <Row>
-                  <Col>Total</Col>
+                  <Col>Total:</Col>
                   <Col>₱{order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
