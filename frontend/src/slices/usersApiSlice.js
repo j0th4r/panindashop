34cc74a2de02208f8f17443214b21dc56ca01931
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-05 17:16:42 +0800

    Add list users for admin

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -29,7 +29,14 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: 'PUT',
         body: data,
       }),
-    })
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ['Users'],
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -38,4 +45,5 @@ export const {
   useLogoutMutation, 
   useRegisterMutation, 
   useProfileMutation, 
+  useGetUsersQuery,
 } = usersApiSlice;
