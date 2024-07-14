8134ad6a2fdf1a9ec924039ee9c6fc5d3e17aa67
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-14 21:16:20 +0800

    Install shadcnui

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { Button } from '../components/ui/button'
 import { useParams, useNavigate } from 'react-router-dom';
 
 const SearchBox = () => {
@@ -27,7 +28,7 @@ const SearchBox = () => {
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'
         ></Form.Control>
-        <Button type='submit' variant='outline-success' className='p-2 mx-2'>
+        <Button type='submit' variant='outline' className='p-2 m-2'>
           Search
         </Button>
     </Form>
